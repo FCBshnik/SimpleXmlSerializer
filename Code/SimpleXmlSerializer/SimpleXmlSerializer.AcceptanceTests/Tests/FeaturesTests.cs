ed79d49a60dec96d7fab131eebb4152444a4ed6e
FCBshnik <fcbshnik.dev@gmail.com>
2014-07-08 22:42:49 +0400

    rename

--- Code/SimpleXmlSerializer/SimpleXmlSerializer.AcceptanceTests/Tests/FeaturesTests.cs
+++ Code/SimpleXmlSerializer/SimpleXmlSerializer.AcceptanceTests/Tests/FeaturesTests.cs
@@ -1,8 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleXmlSerializer.AcceptanceTests.Dto.Football;
-using SimpleXmlSerializer.AcceptanceTests.Tests.Features;
 using SimpleXmlSerializer.AcceptanceTests.Utils;
+using SimpleXmlSerializer.Core;
 
 namespace SimpleXmlSerializer.AcceptanceTests.Tests
 {
@@ -67,6 +69,25 @@ namespace SimpleXmlSerializer.AcceptanceTests.Tests
             var path = Path.Combine(AssetsDirectory, fileName + ".xml");
 
             serializer.DeserializeAndAssertObject(expected, path);
+        }
+
+        public class CoachCustomSerializer : ICustomNodeSerializer
+        {
+            public void Serialize(object value, XmlWriter xmlWriter)
+            {
+                var coach = (Coach)value;
+
+                xmlWriter.WriteValue(string.Format("{0};{1}", coach.Name, coach.Age));
+            }
+
+            public object Deserialize(XmlReader xmlReader)
+            {
+                var serializedValue = xmlReader.ReadElementString();
+
+                var parts = serializedValue.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+                return new Coach { Name = parts[0], Age = int.Parse(parts[1]) };
+            }
         }
     }
 }
